157d61e775ef3b92e84e4bb76c535bcdc5c7d82a
Marcel Cohen <diegomarcel27@hotmail.com>
2018-04-19 21:46:51 -0300

    se agregan mas tests a clase Point para equals, se contemplan mas casos

--- Obligatorio1_Arancet_Cohen/Logic.Test/PointTest.cs
+++ Obligatorio1_Arancet_Cohen/Logic.Test/PointTest.cs
@@ -34,5 +34,23 @@ namespace Logic.Test
             Point otherInstance = new Point(3, 2);
             Assert.AreEqual(instance,otherInstance);
         }
+
+        [TestMethod]
+        public void NotEqualsTest() {
+            Point otherInstance = new Point(5, 2);
+            Assert.AreEqual(instance, otherInstance);
+        }
+
+        [TestMethod]
+        public void EqualsNullTest() {
+            Point otherInstance = new Point(3, 2);
+            Assert.AreNotEqual(instance, otherInstance);
+        }
+
+        [TestMethod]
+        public void EqualsOtherObjectTest() {
+            Point otherInstance = new Point(3, 2);
+            Assert.AreNotEqual(instance, otherInstance);
+        }
     }
 }
