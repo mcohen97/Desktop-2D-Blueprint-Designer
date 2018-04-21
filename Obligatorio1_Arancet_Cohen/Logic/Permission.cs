736b1c93664d6603c79e4b9a18c1b4d6504d3fc4
Facundo Arancet Iza <facuarancet97@gmail.com>
2018-04-21 12:44:37 -0300

    Se arreglan referencias de clases de usuarios

---
+++ Obligatorio1_Arancet_Cohen/Logic/Permission.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obligatorio1_Arancet_Cohen
+{
+    public enum Permission
+    {
+        ALL_PERMISSIONS,
+        READ_BLUEPRINT,
+        EDIT_BLUEPRINT,
+        DELETE_BLUEPRINT,
+        CREATE_BLUEPRINT,
+        EDIT_USER,
+        DELETE_USER,
+        CREATE_USER
+    }
+}
