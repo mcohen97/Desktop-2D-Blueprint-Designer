e8c5bbf3a98d5cacf7c47eef5c2c605fcf07351a
Marcel Cohen <diegomarcel27@hotmail.com>
2018-04-30 17:32:51 -0300

    se avanza en la implementacion de Blueprint

--- Obligatorio1_Arancet_Cohen/Logic/Blueprint.cs
+++ Obligatorio1_Arancet_Cohen/Logic/Blueprint.cs
@@ -7,8 +7,8 @@ using System.Threading.Tasks;
 namespace Logic {
     public class Blueprint {
 
-        private int Length;
-        private int Width;
+        private int Length;//Horizontal X Mesaure
+        private int Width;//Vertical Y Mesaure
         private BuildingComponentContainer materials;
 
         public Blueprint(int aLength, int aWidth) {
@@ -21,13 +21,37 @@ namespace Logic {
         }
 
         public void InsertWall(Wall aWall) {
-            if (!InRange(aWall.Beginning) || !InRange(aWall.End)) {
-
+            if (!InRange(aWall.Beginning()) || !InRange(aWall.End())) {
+                throw new OutOfRangeComponentException();
             } else {
-
-
+                if (!IntersectsOtherWalls(aWall)) {
+                    materials.addWall(aWall);
+                }
             }
         }
+
+        private bool InRange(Point aPoint) {
+            bool xInRange = aPoint.CoordX >= 0 && aPoint.CoordY <= Length;
+            bool yInRange = aPoint.CoordY >= 0 && aPoint.CoordY <= Width;
+            return xInRange && yInRange;
+        }
+
+        private bool IntersectsOtherWalls(Wall aWall) {
+            return WallsIntersectedByNewOne(aWall).Any();
+        }
+
+        private ICollection<Wall> WallsIntersectedByNewOne(Wall newWall) {
+            List<Wall> intersectedWalls = new List<Wall>();
+             
+            foreach (Wall existentWall in materials.GetWalls()) {
+                if (existentWall.DoesIntersect(newWall)) {
+                    intersectedWalls.Add(existentWall);
+                }
+            }
+
+            return intersectedWalls;
+        }
+
 
         public void InsertOpening(Opening testOpening) {
             throw new NotImplementedException();
