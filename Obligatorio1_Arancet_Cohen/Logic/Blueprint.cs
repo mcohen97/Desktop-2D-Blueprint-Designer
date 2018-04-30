4659886ba0d37815321f128de99b4f3a1241795e
Marcel Cohen <diegomarcel27@hotmail.com>
2018-04-30 19:05:14 -0300

    se avanzo en la implementacion de la clase BluePrint

--- Obligatorio1_Arancet_Cohen/Logic/Blueprint.cs
+++ Obligatorio1_Arancet_Cohen/Logic/Blueprint.cs
@@ -14,20 +14,65 @@ namespace Logic {
         public Blueprint(int aLength, int aWidth) {
             Length = aLength;
             Width = aWidth;
+            materials = new BuildingComponentContainer();
         }
 
         public BuildingComponentContainer GetComponentsContainer() {
-            throw new NotImplementedException();
+            return materials;
         }
 
-        public void InsertWall(Wall aWall) {
-            if (!InRange(aWall.Beginning()) || !InRange(aWall.End())) {
+        public void InsertWall(Wall newWall) {
+            if (!InRange(newWall.Beginning()) || !InRange(newWall.End())) {
                 throw new OutOfRangeComponentException();
             } else {
-                if (!IntersectsOtherWalls(aWall)) {
-                    materials.addWall(aWall);
+                if (!IntersectsOtherWalls(newWall)) {
+                    PlaceNewWall(newWall);
+                } else {
+                    List<Point> intersectionPoints = new List<Point>();
+                    Point actualIntersection;
+                    foreach (Wall intersected in WallsIntersectedByThisOne(newWall)) {
+                        actualIntersection = intersected.GetIntersection(newWall);
+                        Wall firstHalf = new Wall(intersected.Beginning(),actualIntersection);
+                        Wall secondHalf = new Wall(actualIntersection, intersected.End());
+                        materials.RemoveWall(intersected); // we are replacing the old walls with two halfs
+                        AdjustWall(firstHalf);
+                        AdjustWall(secondHalf);
+                        intersectionPoints.Add(actualIntersection);
+                    }
+                    intersectionPoints.Sort();
+                    SplitWall(newWall, intersectionPoints);
                 }
             }
+        }
+
+        private void PlaceNewWall(Wall newWall) {
+            Beam BeginningBeam = new Beam(newWall.Beginning());
+            Beam EndBeam = new Beam(newWall.End());
+            PlaceBeamIfNotExists(BeginningBeam);
+            PlaceBeamIfNotExists(EndBeam);
+            materials.addWall(newWall);
+        }
+
+        private void PlaceBeamIfNotExists(Beam aBeam) {
+            if (!materials.ContainsBeam(aBeam)) {
+                materials.AddBeam(aBeam);
+            }
+        }
+
+        private void SplitWall(Wall newWall, List<Point> intersectionPoints) {
+            Wall firstPiece = new Wall(newWall.Beginning(), intersectionPoints.First());
+            AdjustWall(firstPiece);
+            Wall actualPiece;
+            for (int i = 1; i < intersectionPoints.Count-1; i++) {
+                actualPiece = new Wall(intersectionPoints[i], intersectionPoints[i + 1]);
+                AdjustWall(actualPiece);
+            }
+            Wall lastPiece = new Wall(intersectionPoints.Last(), newWall.End());
+            AdjustWall(lastPiece);
+        }
+
+        private void AdjustWall(Wall generatedWall) {
+            InsertWall(generatedWall);//pending refactory
         }
 
         private bool InRange(Point aPoint) {
@@ -37,10 +82,10 @@ namespace Logic {
         }
 
         private bool IntersectsOtherWalls(Wall aWall) {
-            return WallsIntersectedByNewOne(aWall).Any();
+            return WallsIntersectedByThisOne(aWall).Any();
         }
 
-        private ICollection<Wall> WallsIntersectedByNewOne(Wall newWall) {
+        private ICollection<Wall> WallsIntersectedByThisOne(Wall newWall) {
             List<Wall> intersectedWalls = new List<Wall>();
              
             foreach (Wall existentWall in materials.GetWalls()) {
