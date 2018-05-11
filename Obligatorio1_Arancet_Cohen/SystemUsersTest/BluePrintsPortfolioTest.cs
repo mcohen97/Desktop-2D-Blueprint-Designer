ef3f80797c06b603e70c14272c36e3e50a135c53
Facundo Arancet Iza <facuarancet97@gmail.com>
2018-05-11 13:14:39 -0300

    Pruebas de BlueprintPortfolio ok

--- Obligatorio1_Arancet_Cohen/SystemUsersTest/BluePrintsPortfolioTest.cs
+++ Obligatorio1_Arancet_Cohen/SystemUsersTest/BluePrintsPortfolioTest.cs
@@ -1,31 +1,24 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Logic;
+using System.Collections.Generic;
 
 namespace SystemUsersTest {
     [TestClass]
-    public class BluePrintPortfolioTest
-        {
+    public class BluePrintPortfolioTest {
 
         private IBlueprint blueprint1;
         private IBlueprint blueprint2;
         private IBlueprint blueprint3;
-        private IBlueprint blueprint4;
-        private User user1;
-        private User user2;
-        private User user3;
         private BlueprintPortfolio portfolio;
-       
+
 
         [TestInitialize]
         public void TestInitialize() {
-            portfolio = UsersPortfolio.Instance;
+            portfolio = BlueprintPortfolio.Instance;
             portfolio.Empty();
-            user1 = new Client("client1N", "client1S", "client1UN", "client1P", "999000111", "dir", "55555555", DateTime.Now);
-            user2 = new Client("client2N", "client2S", "client2UN", "client2P", "999000111", "dir", "55555556", DateTime.Now);
-            user3 = new Designer("designer1N", "designer1S", "designer1UN", "designer1P", DateTime.Now);
 
-            blueprint1 = new Blueprint(12,12);
+            blueprint1 = new Blueprint(12, 12);
             blueprint2 = new Blueprint(12, 12);
             blueprint3 = new Blueprint(12, 12);
         }
@@ -37,72 +30,45 @@ namespace SystemUsersTest {
         }
 
         [TestMethod]
-        public void AddUserTest() {
-            portfolio.Add(user1);
+        public void AddBlueprintTest() {
+            portfolio.Add(blueprint1);
             Assert.IsFalse(portfolio.IsEmpty());
         }
 
         [TestMethod]
-        public void AddedUserTest() {
-            portfolio.Add(user1);
-            Assert.IsTrue(portfolio.Exist(user1));
+        public void AddedBlueprintTest() {
+            portfolio.Add(blueprint1);
+            Assert.IsTrue(portfolio.Exist(blueprint1));
         }
 
         [TestMethod]
-        public void GetUsersTest() {
-            portfolio.Add(user1);
-            IEnumerator<User> users = portfolio.GetEnumerator();
-            users.MoveNext();
-            Assert.IsNotNull(users.Current);
+        public void GetBlueprintTest() {
+            portfolio.Add(blueprint1);
+            IEnumerator<IBlueprint> blueprints = portfolio.GetEnumerator();
+            blueprints.MoveNext();
+            Assert.IsNotNull(blueprints.Current);
         }
 
         [TestMethod]
-        public void RemoveExistentUserTest() {
-            portfolio.Add(user1);
-            portfolio.Add(user2);
-            portfolio.Remove(user1);
-            Assert.IsFalse(portfolio.Exist(user1));
+        public void RemoveExistentBlueprintTest() {
+            portfolio.Add(blueprint1);
+            portfolio.Add(blueprint2);
+            portfolio.Remove(blueprint1);
+            Assert.IsFalse(portfolio.Exist(blueprint1));
         }
 
         [TestMethod]
-        public void RemoveNonExistentUserTest() {
-            portfolio.Add(user2);
-            bool deletionExecuted = portfolio.Remove(user1);
+        public void RemoveNonExistentBlueprintTest() {
+            portfolio.Add(blueprint2);
+            bool deletionExecuted = portfolio.Remove(blueprint1);
             Assert.IsFalse(deletionExecuted);
         }
 
         [TestMethod]
-        public void GetClientTest() {
-            portfolio.Add(user2);
-            Client clientInfo = portfolio.GetClient(user2);
-            Assert.AreEqual(user2, clientInfo);
-        }
-
-        [TestMethod]
-        public void GetDesignerTest() {
-            portfolio.Add(user3);
-            Designer designerInfo = portfolio.GetDesigner(user3);
-            Assert.AreEqual(user3, designerInfo);
-        }
-
-        [TestMethod]
-        public void GetAdminTest() {
-            portfolio.Add(user5);
-            Admin clientInfo = portfolio.GetAdmin(user5);
-            Assert.AreEqual(user5, clientInfo);
-        }
-
-        [TestMethod]
-        public void GetUserTest() {
-            portfolio.Add(user5);
-            User userInfo = portfolio.GetUser(user5);
-            Assert.AreEqual(user5, userInfo);
-        }
-
-        [TestMethod]
-        public void GetUserByUserNameTest() {
-            portfolio.Add(user5);
-            User userInfo = portfolio.GetUserByUserName(user5.UserName);
-            Assert.AreEqual(user5, userInfo);
+        public void GetEnumeratorBlueprintTest() {
+            portfolio.Add(blueprint1);
+            IBlueprint blueprintGot = portfolio.GetBlueprint(blueprint1);
+            Assert.AreEqual(blueprintGot, blueprint1);
         }
     }
+}
