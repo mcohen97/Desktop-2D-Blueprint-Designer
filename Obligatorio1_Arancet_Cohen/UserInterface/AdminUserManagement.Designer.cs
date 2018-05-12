6d75d7411517a636e479f967d7d916e648c278a7
Marcel Cohen <diegomarcel27@hotmail.com>
2018-05-11 22:13:44 -0300

    se crea la clase user management que le permite al usuario editar y borrar usuarios

---
+++ Obligatorio1_Arancet_Cohen/UserInterface/AdminUserManagement.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace UserInterface {
+    partial class AdminUserManagement {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent() {
+            this.userList = new System.Windows.Forms.ListBox();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.doneButton = new System.Windows.Forms.Button();
+            this.editButton = new System.Windows.Forms.Button();
+            this.errorLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // userList
+            // 
+            this.userList.FormattingEnabled = true;
+            this.userList.Location = new System.Drawing.Point(206, 29);
+            this.userList.Name = "userList";
+            this.userList.Size = new System.Drawing.Size(348, 316);
+            this.userList.TabIndex = 0;
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(373, 383);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(142, 47);
+            this.deleteButton.TabIndex = 5;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // doneButton
+            // 
+            this.doneButton.Location = new System.Drawing.Point(716, 383);
+            this.doneButton.Name = "doneButton";
+            this.doneButton.Size = new System.Drawing.Size(142, 47);
+            this.doneButton.TabIndex = 4;
+            this.doneButton.Text = "Done";
+            this.doneButton.UseVisualStyleBackColor = true;
+            this.doneButton.Click += new System.EventHandler(this.doneButton_Click);
+            // 
+            // editButton
+            // 
+            this.editButton.Location = new System.Drawing.Point(206, 383);
+            this.editButton.Name = "editButton";
+            this.editButton.Size = new System.Drawing.Size(142, 47);
+            this.editButton.TabIndex = 6;
+            this.editButton.Text = "Edit";
+            this.editButton.UseVisualStyleBackColor = true;
+            this.editButton.Click += new System.EventHandler(this.editButton_Click);
+            // 
+            // errorLabel
+            // 
+            this.errorLabel.AutoSize = true;
+            this.errorLabel.Location = new System.Drawing.Point(539, 400);
+            this.errorLabel.Name = "errorLabel";
+            this.errorLabel.Size = new System.Drawing.Size(0, 13);
+            this.errorLabel.TabIndex = 7;
+            // 
+            // AdminUserManagement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.errorLabel);
+            this.Controls.Add(this.editButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.doneButton);
+            this.Controls.Add(this.userList);
+            this.Name = "AdminUserManagement";
+            this.Size = new System.Drawing.Size(884, 461);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox userList;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button doneButton;
+        private System.Windows.Forms.Button editButton;
+        private System.Windows.Forms.Label errorLabel;
+    }
+}
---
+++ Obligatorio1_Arancet_Cohen/UserInterface/AdminUserManagement.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Logic;
+
+namespace UserInterface {
+    public partial class AdminUserManagement : UserControl, IUserFeatureControl {
+
+        private Session CurrentSession { get; set; }
+        private LoggedInView parent;
+
+        public AdminUserManagement(Session aSession, LoggedInView aControl) {
+            InitializeComponent();
+            parent = aControl;
+            CurrentSession = aSession;
+            FillList();
+        }
+
+        private void FillList() {
+            userList.DataSource = null;
+            List<User> elegibleUsers = UsersPortfolio.Instance.GetUsers().ToList();
+            elegibleUsers.Remove(CurrentSession.UserLogged);
+            userList.DataSource = elegibleUsers;
+        }
+
+        private void editButton_Click(object sender, EventArgs e) {
+            bool selectedItem = InputValidations.IsListItemSelected(userList, errorLabel,
+               "Debe seleccionar un usuario primero");
+            if (selectedItem) {
+                parent.OpenUserEditor((User)userList.SelectedItem);
+            }
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e) {
+            bool selectedItem=InputValidations.IsListItemSelected(userList, errorLabel,
+                "Debe seleccionar un usuario primero");
+            if (selectedItem) {
+                UsersPortfolio.Instance.Remove((User)userList.SelectedItem);
+                FillList();
+            }
+        }
+
+        public Permission GetRequiredPermission() {
+            return Permission.EDIT_USER;
+        }
+
+        public Button OptionMenuButton() {
+            return ButtonCreator.GenerateButton("Manage Users");
+        }
+
+        private void doneButton_Click(object sender, EventArgs e) {
+            parent.RestartMenu();
+        }
+    }
+}
