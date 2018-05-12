a181edf9968e46358c5eeba35a8ef24ccf379e6e
Marcel Cohen <diegomarcel27@hotmail.com>
2018-05-11 22:13:06 -0300

    como parte de refactoring, se tomaron metodos que se utilizan en muchas clases y se colocaron estaticos en clases de metodos

---
+++ Obligatorio1_Arancet_Cohen/UserInterface/ButtonCreator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UserInterface {
+    class ButtonCreator {
+
+        public static Button GenerateButton(string title) {
+            Button optionButton = new Button();
+            optionButton.Width = 100;
+            optionButton.Height = 50;
+            optionButton.Text = title;
+            return optionButton;
+        }
+
+    }
+}
---
+++ Obligatorio1_Arancet_Cohen/UserInterface/InputValidations.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+
+namespace UserInterface {
+    class InputValidations {
+
+        public static void ErrorMessage(Label aMessageLabel,string errMessage) {
+            aMessageLabel.ForeColor = Color.Red;
+            aMessageLabel.Text = errMessage;
+        }
+        public static void ValidateIfEmpty(TextBox anInput, Label msgLabel) {
+            if (string.IsNullOrEmpty(anInput.Text)) {
+                ErrorMessage(msgLabel, "Empty field!");
+            } 
+        }
+        public static void ClearField(Label msgLabel) {
+            msgLabel.Text = "";
+        }
+
+        public static bool IsListItemSelected(ListBox aList, Label aMsjLabel,string errorMsg) {
+            bool valid = true;
+            if (aList.SelectedItem == null) {
+                aMsjLabel.ForeColor = Color.Red;
+                aMsjLabel.Text = errorMsg;
+                valid = false;
+            }
+            return valid;
+        }
+
+
+
+    }
+}
