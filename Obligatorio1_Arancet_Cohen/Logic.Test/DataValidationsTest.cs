f8b1cab65c68d41be58ef04c32d00acad1b1a79d
Marcel Cohen <diegomarcel27@hotmail.com>
2018-05-13 15:25:35 -0300

    se agrega clase de prueba para los metodos a implementar de una nueva clase

---
+++ Obligatorio1_Arancet_Cohen/Logic.Test/DataValidationsTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Logic.Test {
+    [TestClass]
+    public class DataValidationsTest {
+        [TestMethod]
+        public void AssignStringTest() {
+            string testString = "Hello";
+            DataValidations.AssignStringIfNotNull(out testString, "World!");
+            Assert.AreEqual("World!", testString);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void AssignNullStringTest() {
+            string testString = "Hello";
+            DataValidations.AssignStringIfNotNull(out testString, null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void AssignEmptyStringTest() {
+            string testString = "Hello";
+            DataValidations.AssignStringIfNotNull(out testString, "");
+        }
+
+        [TestMethod]
+        public void ValidPhoneNumberTest() {
+            string testPhone = "2400-38-85";
+            Assert.IsTrue(DataValidations.IsValidPhoneNumber(testPhone));
+        }
+
+        [TestMethod]
+        public void InvalidPhoneNumberTest() {
+            string testPhone = "1-555-5555";
+            Assert.IsFalse(DataValidations.IsValidPhoneNumber(testPhone));
+        }
+
+        [TestMethod]
+        public void ValidIDTest() {
+            string testPhone = "4.972.519-2";
+            Assert.IsFalse(DataValidations.IsValidID(testPhone));
+        }
+
+        [TestMethod]
+        public void InvalidIDTest() {
+            string testPhone = "1-555-5555";
+            Assert.IsFalse(DataValidations.IsValidID(testPhone));
+        }
+
+    }
+}
