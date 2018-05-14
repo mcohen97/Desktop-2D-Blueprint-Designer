d72369c9b59313224f901bea96b1543e8048493d
Marcel Cohen <diegomarcel27@hotmail.com>
2018-05-14 00:01:39 -0300

    se edito el usuario de prueba, ya que la cedula y el telefono ahora tienen un formato

--- Obligatorio1_Arancet_Cohen/Logic/UsersPortfolio.cs
+++ Obligatorio1_Arancet_Cohen/Logic/UsersPortfolio.cs
@@ -18,7 +18,7 @@ namespace Logic {
                 if(instance == null) {
                     instance = new UsersPortfolio();
                     instance.Add(new Admin("admin", "admin", "admin", "admin", DateTime.Now));
-                    instance.Add(new Client("user","user","user","user","user","user","user",DateTime.Now));
+                    instance.Add(new Client("user","user","user","user","0800-21-08","user","1.111.111-2",DateTime.Now));
                     instance.Add(new Designer("des", "des", "des", "des", DateTime.Now));
                 }
                 return instance;
