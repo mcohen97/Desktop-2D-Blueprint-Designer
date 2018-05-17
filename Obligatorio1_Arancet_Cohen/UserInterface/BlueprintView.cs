8d5ef1e5e1dde7a03bc04cbff35f5a470d304858
Facundo Arancet Iza <facuarancet97@gmail.com>
2018-05-17 11:06:41 -0300

    Se agrega jpeg como uno de los formatos en los que se puede exportar el plano

--- Obligatorio1_Arancet_Cohen/UserInterface/BlueprintView.cs
+++ Obligatorio1_Arancet_Cohen/UserInterface/BlueprintView.cs
@@ -327,11 +327,18 @@ namespace UserInterface {
             drawSurface.DrawToBitmap(bitmapToExport, new Rectangle(0, 0, width, height));
 
             SaveFileDialog saveFile = new SaveFileDialog();
-            saveFile.Filter = "Png Image (.png)|*.png";
+            saveFile.Filter = "JPEG Image (.jpeg)|*.jpeg|Png Image (.png)|*.png";
             saveFile.ShowDialog();
             var path = saveFile.FileName;
 
-            bitmapToExport.Save(path, ImageFormat.Png);
+            ImageFormat imageFormatSelected = ImageFormat.Png;
+            if (saveFile.Filter == ".jpeg") {
+                imageFormatSelected = ImageFormat.Jpeg;
+            } else if (saveFile.Filter == ".png") {
+                imageFormatSelected = ImageFormat.Png;
+            }
+
+            bitmapToExport.Save(path, imageFormatSelected);
         }
 
         private void btnZoomIn_Click(object sender, EventArgs e) {
