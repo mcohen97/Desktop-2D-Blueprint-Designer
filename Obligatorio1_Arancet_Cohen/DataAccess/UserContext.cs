8256d8c49c3fcfc11b78bb66c5d30d9c9081becd
Marcel Cohen <diegomarcel27@hotmail.com>
2018-06-01 20:44:49 -0300

    se crea UserContext

---
+++ Obligatorio1_Arancet_Cohen/DataAccess/UserContext.cs
@@ -0,0 +1,27 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataAccess
+{
+    class UserContext : DbContext
+    {
+        public DbSet<UserEntity> Users { get; set; }
+        public UserContext() : base("name=UserContext")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<UserEntity>().Property(x => x.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+        }
+    }
+}
