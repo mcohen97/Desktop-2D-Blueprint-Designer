b16f0327d700e9b46a11a24d7cdf60fab1598ebe
Marcel Cohen <diegomarcel27@hotmail.com>
2018-06-04 01:01:48 -0300

    se agregan metodos al userRepository

--- Obligatorio1_Arancet_Cohen/DataAccess/UserRepository.cs
+++ Obligatorio1_Arancet_Cohen/DataAccess/UserRepository.cs
@@ -12,22 +12,65 @@ namespace DataAccess
 {
     public class UserRepository : IRepository<UserEntity>
     {
-        public void Add(UserEntity entity)
+
+        public bool IsEmpty() {
+            bool esVacia;
+            using (BlueBuilderDBContext context = new BlueBuilderDBContext())
+            {
+                esVacia=context.Users.Any();
+            }
+            return esVacia;
+        }
+
+        public void Add(UserEntity record)
         {
             using (BlueBuilderDBContext context = new BlueBuilderDBContext())
             {
-                context.Users.Add(entity);
+                context.Users.Add(record);
                 context.SaveChanges();
             }
         }
 
-        public void Delete(UserEntity entity)
+        public void Delete(UserEntity record)
         {
             using (BlueBuilderDBContext context = new BlueBuilderDBContext())
             {
-                context.Users.Remove(entity);
+                context.Users.Remove(record);
                 context.SaveChanges();
             }
+        }
+
+        public bool Exists(UserEntity record) {
+            bool doesExist;
+
+            using (BlueBuilderDBContext context = new BlueBuilderDBContext())
+            {
+                doesExist=context.Users.Contains(record);
+            }
+
+            return doesExist;
+        }
+
+        public void Clear() {
+            using (BlueBuilderDBContext context = new BlueBuilderDBContext())
+            {
+                foreach (var userEnt in context.Users)
+                {
+                    context.Users.Remove(userEnt);
+                }
+                context.SaveChanges();
+            }
+
+        }
+
+        public bool ExistsUserName(string aUserName) {
+            bool doesExist;
+
+            using (BlueBuilderDBContext context = new BlueBuilderDBContext()) {
+                doesExist = context.Users.Any(u => u.UserName.Equals(aUserName));
+            }
+
+            return doesExist;
         }
 
         public UserEntity Get(Guid id)
@@ -38,6 +81,30 @@ namespace DataAccess
             }
         }
 
+
+        public UserEntity GetUserByUserName(string userName)
+        {
+            UserEntity record;
+            using (BlueBuilderDBContext context = new BlueBuilderDBContext())
+            {
+                record = context.Users.First(ue => ue.UserName.Equals(userName));
+            }
+            return record;
+
+        }
+
+        public IEnumerable<UserEntity> GetUsersByPermission(int aFeature)
+        {
+            IQueryable<UserEntity> elegibleRecords;
+            using (BlueBuilderDBContext context = new BlueBuilderDBContext())
+            {
+                elegibleRecords = context.Users.Where(ue => ue.Permissions.Contains(aFeature));
+            }
+
+            return elegibleRecords;
+        }
+
+
         public IEnumerable<UserEntity> GetAll()
         {
             using (BlueBuilderDBContext context = new BlueBuilderDBContext())
@@ -46,11 +113,11 @@ namespace DataAccess
             }
         }
 
-        public void Modify(UserEntity entity)
+        public void Modify(UserEntity record)
         {
             using (BlueBuilderDBContext context = new BlueBuilderDBContext())
             {
-                context.Entry(entity).State = EntityState.Modified;
+                context.Entry(record).State = EntityState.Modified;
                 context.SaveChanges();
             }
         }
