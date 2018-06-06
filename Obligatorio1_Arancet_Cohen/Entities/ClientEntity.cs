44eacbc839e139dc54513903fe741fd9227ee270
Marcel Cohen <diegomarcel27@hotmail.com>
2018-06-06 17:04:19 -0300

    se crea una entidad para cada subtipo de user y para wall

---
+++ Obligatorio1_Arancet_Cohen/Entities/ClientEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class ClientEntity: UserEntity
+    {
+        public string Phone { get; set; }
+
+        public string IdCard { get; set; }
+
+        public string Address { get; set; }
+
+    }
+}
