d17d233f346737baec4b72a2e9fce31c1ea426a8
Marcel Cohen <diegomarcel27@hotmail.com>
2018-06-06 17:03:16 -0300

    se crea proyecto UserRepositoryInterface, que contiene la interfaz para funciones especificas de un repositorio de usuarios

---
+++ Obligatorio1_Arancet_Cohen/UserRepositoryInterface/IUserRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logic.Domain;
+
+namespace RepositoryInterface
+{
+    public interface IUserRepository
+    {
+        void AddUser(Admin anAdmin);
+
+        void AddUser(Client aClient);
+
+        void AddUser(Designer aDesigner);
+
+        bool ExistsUserName(string aUserName);
+
+        User GetUserByUserName(string userName);
+
+        IEnumerable<User> GetUsersByPermission(int aFeature);
+
+
+
+    }
+}
