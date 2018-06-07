609e248def67e398affd8d0339c9ce520285c464
Marcel Cohen <diegomarcel27@hotmail.com>
2018-06-06 22:57:30 -0300

    se mueven clases de prueba al proyecto dataAccessTest

--- Obligatorio1_Arancet_Cohen/DataAccessTest/UsersRepositoryTest.cs
+++ Obligatorio1_Arancet_Cohen/DataAccessTest/UsersRepositoryTest.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Logic.Domain;
 using Logic.Exceptions;
+using DataAccess;
 using System.Collections.Generic;
 
 namespace Logic.Test
@@ -14,13 +15,13 @@ namespace Logic.Test
         private User user3;
         private User user4;
         private User user5;
-        private UsersPortfolio portfolio;
+        private UserRepository repository;
 
         [TestInitialize]
         public void TestInitialize()
         {
-            portfolio = UsersPortfolio.Instance;
-            portfolio.Clear();
+            repository = new UserRepository();
+            repository.Clear();
             user1 = new Client("client1N", "client1S", "client1UN", "client1P", "999000111", "dir", "55555555", DateTime.Now);
             user2 = new Client("client2N", "client2S", "client2UN", "client2P", "999000111", "dir", "55555556", DateTime.Now);
             user3 = new Designer("designer1N", "designer1S", "designer1UN", "designer1P", DateTime.Now);
@@ -31,101 +32,100 @@ namespace Logic.Test
         [TestMethod]
         public void EmptyPorfolioTest()
         {
-            portfolio.Clear();
-            Assert.IsTrue(portfolio.IsEmpty());
+            repository.Clear();
+            Assert.IsTrue(repository.IsEmpty());
         }
 
         [TestMethod]
         public void AddUserTest()
         {
-            portfolio.Add(user1);
-            Assert.IsFalse(portfolio.IsEmpty());
+            repository.Add(user1);
+            Assert.IsFalse(repository.IsEmpty());
         }
 
         [TestMethod]
         public void AddedUserTest()
         {
-            portfolio.Add(user1);
-            Assert.IsTrue(portfolio.Exists(user1));
+            repository.Add(user1);
+            Assert.IsTrue(repository.Exists(user1));
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentNullException))]
         public void AddNullUserTest()
         {
-            portfolio.Add(null);
+            repository.Add(null);
         }
 
         [TestMethod]
         [ExpectedException(typeof(UserAlreadyExistsException))]
         public void AddExistingUser()
         {
-            portfolio.Add(user1);
-            portfolio.Add(user1);
+            repository.Add(user1);
+            repository.Add(user1);
         }
 
         [TestMethod]
         public void UserNameExistsTest()
         {
-            portfolio.Add(user1);
-            Assert.IsTrue(portfolio.ExistsUserName("client1UN"));
+            repository.Add(user1);
+            Assert.IsTrue(repository.ExistsUserName("client1UN"));
         }
 
         [TestMethod]
         public void UserNameDoesNotExist()
         {
-            Assert.IsFalse(portfolio.ExistsUserName("client1UN"));
+            Assert.IsFalse(repository.ExistsUserName("client1UN"));
         }
 
         [TestMethod]
         public void RemoveExistentUserTest()
         {
-            portfolio.Add(user1);
-            portfolio.Add(user2);
-            portfolio.Delete(user1);
-            Assert.IsFalse(portfolio.Exists(user1));
+            repository.Add(user1);
+            repository.Add(user2);
+            repository.Delete(user1);
+            Assert.IsFalse(repository.Exists(user1));
         }
 
         [TestMethod]
         public void RemoveNonExistentUserTest()
         {
-            portfolio.Add(user2);
-            bool deletionExecuted = portfolio.Delete(user1);
-            Assert.IsFalse(deletionExecuted);
+            repository.Add(user2);
+            repository.Delete(user1);
         }
 
         [TestMethod]
         public void GetUserTest()
         {
-            portfolio.Add(user5);
-            User userInfo = portfolio.Get(user5);
+            repository.Add(user5);
+            User userInfo = repository.Get(user5);
             Assert.AreEqual(user5, userInfo);
         }
 
         [TestMethod]
         public void GetUserByUserNameTest()
         {
-            portfolio.Add(user5);
-            User userInfo = portfolio.GetUserByUserName(user5.UserName);
+            repository.Add(user5);
+            User userInfo = repository.GetUserByUserName(user5.UserName);
             Assert.AreEqual(user5, userInfo);
         }
 
         [TestMethod]
         public void GetAllUsersTest()
         {
-            portfolio.Add(user1);
-            portfolio.Add(user2);
+            repository.Add(user1);
+            repository.Add(user2);
             int expectedResult = 3;
-            int actualResult = portfolio.GetAll().Count;
+            int actualResult = repository.GetAll().Count;
             Assert.AreEqual(expectedResult, actualResult);
         }
 
         [TestMethod]
         public void GetUsersByPermissionTest()
         {
-            portfolio.Add(user1);
-            portfolio.Add(user3);
-            ICollection<User> filtered = portfolio.GetUsersByPermission(Permission.READ_BLUEPRINT);
+            repository.Add(user1);
+            repository.Add(user3);
+            ICollection<User> filtered = repository.GetUsersByPermission((int)Permission.READ_BLUEPRINT);
             int expectedResult = 2;
             int actualResult = filtered.Count;
             Assert.AreEqual(expectedResult, actualResult);
@@ -134,9 +134,9 @@ namespace Logic.Test
         [TestMethod]
         public void RecursiveBlueprintDeletionTest()
         {
-            portfolio.Add(user1);
-            portfolio.Add(user2);
-            portfolio.Add(user3);
+            repository.Add(user1);
+            repository.Add(user2);
+            repository.Add(user3);
 
             Blueprint blueprint1 = new Blueprint(12, 12, "Blueprint1");
             blueprint1.Owner = user1;
@@ -148,7 +148,7 @@ namespace Logic.Test
             BlueprintPortfolio.Instance.Add(blueprint2);
             BlueprintPortfolio.Instance.Add(blueprint3);
 
-            portfolio.Delete(user1);
+            repository.Delete(user1);
             int expectedResult = 1;
             int actualResult = BlueprintPortfolio.Instance.GetBlueprintsCopy().Count;
             Assert.AreEqual(expectedResult, actualResult);
