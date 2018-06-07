6e9d8ff291c3db68b947ae51ff977c47e454c4b1
Marcel Cohen <diegomarcel27@hotmail.com>
2018-06-07 14:03:12 -0300

    se resuelve el problema de las fechas en la base de datos

--- Obligatorio1_Arancet_Cohen/DataAccess/BlueBuilderDBContext.cs
+++ Obligatorio1_Arancet_Cohen/DataAccess/BlueBuilderDBContext.cs
@@ -20,7 +20,8 @@ namespace DataAccess
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-
+            modelBuilder.Entity<UserEntity>().Property(u => u.RegistrationDate).HasColumnType("datetime2");
+            modelBuilder.Entity<UserEntity>().Property(u => u.LastLoginDate).HasColumnType("datetime2");
             modelBuilder.Entity<UserEntity>().Property(x => x.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
         }
     }
