93101dd72f9c25450c94e783d1e9404493ba17d0
Facundo Arancet Iza <facuarancet97@gmail.com>
2018-06-06 21:27:59 -0300

    Se agrega la clase Signature

---
+++ Obligatorio1_Arancet_Cohen/Logic.Test/SignatureTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Logic.Domain;
+
+namespace Logic.Test
+{
+    [TestClass]
+    public class SignatureTest
+    {
+        private User user;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            user = new Architect("Jorge", "Jamil", "jjmil", "12345", DateTime.Now);
+        }
+
+        [TestMethod]
+        public void CreateSignatureTest()
+        {
+            Signature signature = new Signature(user);
+
+            Assert.IsNotNull(signature);
+        }
+
+        [TestMethod]
+        public void GetUserTest()
+        {
+            Signature signature = new Signature(user);
+            User architectGet = signature.User;
+
+            Assert.AreEqual(user, architectGet);
+        }
+
+        [TestMethod]
+        public void GetDateTest()
+        {
+            Signature signature = new Signature(user);
+            User architectGet = signature.User;
+
+            Assert.AreEqual(user, architectGet);
+        }
+    }
+}
---
+++ Obligatorio1_Arancet_Cohen/Logic/Signature.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logic.Domain;
+
+namespace Logic
+{
+    class Signature
+    {
+        private DateTime now;
+        private User user;
+
+        public Signature(User user, DateTime now)
+        {
+            this.user = user;
+            this.now = now;
+        }
+
+        public DateTime Date { get; internal set; }
+        public User User { get; internal set; }
+    }
+}
