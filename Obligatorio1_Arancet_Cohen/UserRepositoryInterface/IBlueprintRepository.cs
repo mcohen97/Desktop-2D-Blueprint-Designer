0e7dc9615fa4ce9a819b68a9db450eec801a0e93
Marcel Cohen <diegomarcel27@hotmail.com>
2018-06-07 15:37:26 -0300

    se especifica la interfaz

--- Obligatorio1_Arancet_Cohen/UserRepositoryInterface/IBlueprintRepository.cs
+++ Obligatorio1_Arancet_Cohen/UserRepositoryInterface/IBlueprintRepository.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Logic.Domain;
 
 namespace UserRepositoryInterface
 {
     public interface IBlueprintRepository
     {
+        ICollection<IBlueprint> GetBlueprintsOfUser(User owner);
+
+        void DeleteUserBlueprints(Client aUser);
     }
 }
