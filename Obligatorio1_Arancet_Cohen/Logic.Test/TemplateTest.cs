5132d73eecf351bfa3be2aacf918ac10802e1584
Facundo Arancet Iza <facuarancet97@gmail.com>
2018-06-09 10:30:34 -0300

    Pruebas de Template verdes

---
+++ Obligatorio1_Arancet_Cohen/Logic.Test/TemplateTest.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Logic.Domain;
+
+namespace Logic.Test
+{
+    [TestClass]
+    public class TemplateTest
+    {
+        [TestMethod]
+        public void TemplateConstructorTest()
+        {
+            string name = "My custom template";
+            float length = 1;
+            float width = 0.5f;
+            float height = 1;
+            ComponentType type = ComponentType.WINDOW;
+            Template template = new Template(name, length, width, height, type);
+
+            Assert.IsNotNull(template);
+        }
+
+        [TestMethod]
+        public void GetNameTest()
+        {
+            string name = "My custom template";
+            float length = 1;
+            float width = 0.5f;
+            float height = 1;
+            ComponentType type = ComponentType.WINDOW;
+            Template template = new Template(name, length, width, height, type);
+
+            Assert.AreEqual(name, template.Name);
+        }
+
+        [TestMethod]
+        public void GetLengthTest()
+        {
+            string name = "My custom template";
+            float length = 1;
+            float width = 0.5f;
+            float height = 1;
+            ComponentType type = ComponentType.WINDOW;
+            Template template = new Template(name, length, width, height, type);
+
+            Assert.AreEqual(length, template.Length);
+        }
+
+        [TestMethod]
+        public void GetHeightTest()
+        {
+            string name = "My custom template";
+            float length = 1;
+            float width = 0.5f;
+            float height = 1;
+            ComponentType type = ComponentType.WINDOW;
+            Template template = new Template(name, length, width, height, type);
+
+            Assert.AreEqual(height, template.Height);
+        }
+
+        [TestMethod]
+        public void GetWidthTest()
+        {
+            string name = "My custom template";
+            float length = 1;
+            float width = 0.5f;
+            float height = 1;
+            ComponentType type = ComponentType.WINDOW;
+            Template template = new Template(name, length, width, height, type);
+
+            Assert.AreEqual(width, template.Width);
+        }
+
+        [TestMethod]
+        public void GetComponentTypeTest()
+        {
+            string name = "My custom template";
+            float length = 1;
+            float width = 0.5f;
+            float height = 1;
+            ComponentType type = ComponentType.WINDOW;
+            Template template = new Template(name, length, width, height, type);
+
+            Assert.AreEqual(type, template.Type);
+        }
+
+        [TestMethod]
+        public void SetNameTest()
+        {
+            string name = "My custom template";
+            float length = 1;
+            float width = 0.5f;
+            float height = 1;
+            ComponentType type = ComponentType.WINDOW;
+            Template template = new Template(name, length, width, height, type);
+
+            string newName = "New custom name";
+            template.Name = newName;
+
+            Assert.AreEqual(newName, template.Name);
+        }
+
+        [TestMethod]
+        public void SetLengthTest()
+        {
+            string name = "My custom template";
+            float length = 1;
+            float width = 0.5f;
+            float height = 1;
+            ComponentType type = ComponentType.WINDOW;
+            Template template = new Template(name, length, width, height, type);
+
+            float newLength = 2;
+            template.Length = newLength;
+
+            Assert.AreEqual(newLength, template.Length);
+        }
+
+        [TestMethod]
+        public void SetWidthTest()
+        {
+            string name = "My custom template";
+            float length = 1;
+            float width = 0.5f;
+            float height = 1;
+            ComponentType type = ComponentType.WINDOW;
+            Template template = new Template(name, length, width, height, type);
+
+            float newWidth = 0.3f;
+            template.Width = newWidth;
+
+            Assert.AreEqual(newWidth, template.Width);
+        }
+
+        [TestMethod]
+        public void SetHeigthTest()
+        {
+            string name = "My custom template";
+            float length = 1;
+            float width = 0.5f;
+            float height = 1;
+            ComponentType type = ComponentType.WINDOW;
+            Template template = new Template(name, length, width, height, type);
+
+            float newHeight = 0.3f;
+            template.Height = newHeight;
+
+            Assert.AreEqual(newHeight, template.Height);
+        }
+
+        [TestMethod]
+        public void SetTypeTest()
+        {
+            string name = "My custom template";
+            float length = 1;
+            float width = 0.5f;
+            float height = 1;
+            ComponentType type = ComponentType.WINDOW;
+            Template template = new Template(name, length, width, height, type);
+
+            ComponentType newType = ComponentType.DOOR;
+            template.Type = newType;
+
+            Assert.AreEqual(newType, template.Type);
+        }
+    }
+}
---
+++ Obligatorio1_Arancet_Cohen/Logic/Template.cs
@@ -0,0 +1,28 @@
+﻿using Logic.Domain;
+
+namespace Logic
+{
+    public class Template
+    {
+        private float height;
+        private float length;
+        private string name;
+        private ComponentType type;
+        private float width;
+
+        public Template(string name, float length, float width, float height, ComponentType type)
+        {
+            this.name = name;
+            this.length = length;
+            this.width = width;
+            this.height = height;
+            this.type = type;
+        }
+
+        public float Height { get { return this.height; } internal set { this.height = value; } }
+        public float Length { get { return this.length; } internal set { this.length = value; } }
+        public string Name { get { return this.name; } internal set { this.name = value; } }
+        public ComponentType Type { get { return this.type; } internal set { this.type = value; } }
+        public float Width { get { return this.width; } internal set { this.width = value; } }
+    }
+}
