d0b4d00476fabf15e23901936fd99f8e5955c7c4
Marcel Cohen <diegomarcel27@hotmail.com>
2018-06-09 22:24:28 -0300

    se crea clase para la conversion de plano a entidad y viceversa

---
+++ Obligatorio1_Arancet_Cohen/DataAccess/BlueprintAndEntityConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+using Logic.Domain;
+
+namespace DataAccess
+{
+    public class BlueprintAndEntityConverter
+    {
+        public BlueprintEntity BlueprintToEntiy(IBlueprint toConvert) {
+            BlueprintEntity conversion= new BlueprintEntity() {
+                Name= toConvert.
+
+            }
+        }
+
+
+    }
+}
