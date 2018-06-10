1e6a54192a9db3e5f7ac9663fe0a2f63f2eb6e25
Marcel Cohen <diegomarcel27@hotmail.com>
2018-06-09 22:59:18 -0300

    se completo el mapeo entre blueprints y signatures con sus respectivas entidades

--- Obligatorio1_Arancet_Cohen/DataAccess/BlueprintAndEntityConverter.cs
+++ Obligatorio1_Arancet_Cohen/DataAccess/BlueprintAndEntityConverter.cs
@@ -6,15 +6,53 @@ using System.Threading.Tasks;
 using Entities;
 using Logic.Domain;
 
+
 namespace DataAccess
 {
     public class BlueprintAndEntityConverter
     {
-        public BlueprintEntity BlueprintToEntiy(IBlueprint toConvert) {
-            BlueprintEntity conversion= new BlueprintEntity() {
-                Name= toConvert.
+        public BlueprintEntity BlueprintToEntiy(Blueprint toConvert) {
+            UserAndEntityConverter userEntityConverter = new UserAndEntityConverter();
+            BlueprintEntity conversion = new BlueprintEntity()
+            {
+                Name = toConvert.Name,
+                Length = toConvert.Length,
+                Width = toConvert.Width,
+                Owner = userEntityConverter.toEntity(toConvert.Owner),
+                Signatures = (ICollection<SignatureEntity>)toConvert.GetSignatures().Select(s=>SignatureToEntity(s))
 
-            }
+            };
+            return conversion;
+        }
+
+        public Blueprint BlueprintToEntity(BlueprintEntity toConvert) {
+            UserAndEntityConverter userEntityConverter = new UserAndEntityConverter();
+            User convertedUser  = userEntityConverter.toUser(toConvert.Owner);
+            ICollection<Signature> convertedSignatures = (ICollection<Signature>)toConvert.Signatures.Select(s => EntityToSignature(s));
+            Blueprint conversion = new Blueprint(toConvert.Length, toConvert.Width, toConvert.Name, convertedUser, convertedSignatures);
+            return conversion;
+        }
+
+        public SignatureEntity SignatureToEntity(Signature toConvert) {
+
+            UserAndEntityConverter userEntityConverter = new UserAndEntityConverter();
+
+            SignatureEntity conversion = new SignatureEntity()
+            {
+                Signer = userEntityConverter.toEntity(toConvert.Signer),
+                SignatureDate = toConvert.Date
+            };
+
+            return conversion;
+        }
+
+        public Signature EntityToSignature(SignatureEntity toConvert) {
+
+            UserAndEntityConverter userEntityConverter = new UserAndEntityConverter();
+
+            Signature conversion = new Signature(userEntityConverter.toUser(toConvert.Signer), toConvert.SignatureDate);
+            return conversion;
+
         }
 
 
