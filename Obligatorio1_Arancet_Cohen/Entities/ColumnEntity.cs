a3d1ca066086526a522b17caa50aa10e21337666
Marcel Cohen <diegomarcel27@hotmail.com>
2018-06-10 20:11:41 -0300

    se crea clase column entity, que representa la entitdad columna en base

---
+++ Obligatorio1_Arancet_Cohen/Entities/ColumnEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class ColumnEntity
+    {
+        public Guid Id;
+        public float Width { get; set; }
+
+        public float Length { get; set; }
+
+        public float Height { get; set; }
+
+        public PointEntity Position { get; set; }
+
+        public BlueprintEntity BearerBlueprint { get; set; }
+       
+    }
+}
