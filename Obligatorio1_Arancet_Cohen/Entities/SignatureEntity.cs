24b674880f811b854c62054f9b214daee843774a
Marcel Cohen <diegomarcel27@hotmail.com>
2018-06-09 21:34:46 -0300

    se crean entitdades para arquitecto y firma

---
+++ Obligatorio1_Arancet_Cohen/Entities/SignatureEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class SignatureEntity
+    {
+        public Guid Id { get; set; }
+
+        public ArchitectEntity Signer { get; set; }
+
+        public DateTime SignatureDate { get; set; }
+
+    }
+}
