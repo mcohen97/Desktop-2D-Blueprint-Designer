5bee535161e74c641102f17c4597a915be1386af
Marcel Cohen <diegomarcel27@hotmail.com>
2018-06-11 22:38:22 -0300

    se agrega codigo en el convertidor de blueprint a entities

--- Obligatorio1_Arancet_Cohen/DataAccess/BlueprintAndEntityConverter.cs
+++ Obligatorio1_Arancet_Cohen/DataAccess/BlueprintAndEntityConverter.cs
@@ -11,37 +11,41 @@ namespace DataAccess
 {
     public class BlueprintAndEntityConverter
     {
-        public BlueprintEntity BlueprintToEntiy(Blueprint toConvert) {
+        public BlueprintEntity BlueprintToEntiy(IBlueprint toConvert) {
             UserAndEntityConverter userEntityConverter = new UserAndEntityConverter();
+
+            ICollection<SignatureEntity> signatures = toConvert.GetSignatures().Select(s => SignatureToEntity(s)).ToList();
+
             BlueprintEntity conversion = new BlueprintEntity()
             {
                 Name = toConvert.Name,
                 Length = toConvert.Length,
                 Width = toConvert.Width,
                 Owner = userEntityConverter.toEntity(toConvert.Owner),
-                Signatures = (ICollection<SignatureEntity>)toConvert.GetSignatures().Select(s=>SignatureToEntity(s))
+                Signatures = signatures,
+                Id = toConvert.GetId()
 
             };
             return conversion;
         }
 
-        public Blueprint EntityToBlueprint(BlueprintEntity toConvert, ICollection<WallEntity> wallEnts, ICollection<OpeningEntity> openEnts, ICollection<ColumnEntity> colEnts) {
+        public IBlueprint EntityToBlueprint(BlueprintEntity toConvert, ICollection<WallEntity> wallEnts, ICollection<OpeningEntity> openEnts, ICollection<ColumnEntity> colEnts) {
             UserAndEntityConverter userEntityConverter = new UserAndEntityConverter();
 
             User convertedUser  = userEntityConverter.toUser(toConvert.Owner);
-            ICollection<Signature> convertedSignatures = (ICollection<Signature>)toConvert.Signatures.Select(s => EntityToSignature(s));
+            ICollection<Signature> convertedSignatures = toConvert.Signatures.Select(s => EntityToSignature(s)).ToList();
             MaterialContainer materials = BuildUpContainer(wallEnts, openEnts, colEnts);
 
-            Blueprint conversion = new Blueprint(toConvert.Length, toConvert.Width, toConvert.Name, convertedUser,materials, convertedSignatures);
+            IBlueprint conversion = new Blueprint(toConvert.Length, toConvert.Width, toConvert.Name, convertedUser,materials, convertedSignatures,toConvert.Id);
             return conversion;
         }
 
         private MaterialContainer BuildUpContainer(ICollection<WallEntity> wallEnts, ICollection<OpeningEntity> openEnts, ICollection<ColumnEntity> colEnts) {
             MaterialAndEntityConverter translator = new MaterialAndEntityConverter();
 
-            ICollection<Wall> walls = (ICollection<Wall>)wallEnts.Select(w => translator.EntityToWall(w));
-            ICollection<Opening> openings = (ICollection<Opening>)openEnts.Select(o => translator.EntityToOpening(o));
-            ICollection<ISinglePointComponent> columns = (ICollection<ISinglePointComponent>)colEnts.Select(c => translator.EntityToColumn(c));
+            ICollection<Wall> walls = wallEnts.Select(w => translator.EntityToWall(w)).ToList();
+            ICollection<Opening> openings = openEnts.Select(o => translator.EntityToOpening(o)).ToList();
+            ICollection<ISinglePointComponent> columns = colEnts.Select(c => (ISinglePointComponent)translator.EntityToColumn(c)).ToList();
             MaterialContainer container = new MaterialContainer(walls, openings, columns);
             return container;
 
