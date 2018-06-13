02942945babaf13aa3c5ebdc08f1c4759d0074f4
Marcel Cohen <diegomarcel27@hotmail.com>
2018-06-13 15:42:23 -0300

    se agregan pruebas para nueva funcionalidad

--- Obligatorio1_Arancet_Cohen/DataAccessTest/OpeningTemplateRepositoryTest.cs
+++ Obligatorio1_Arancet_Cohen/DataAccessTest/OpeningTemplateRepositoryTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using DataAccess;
+using Logic.Domain;
 
 namespace DataAccessTest
 {
@@ -8,16 +9,40 @@ namespace DataAccessTest
     public class OpeningTemplateRepositoryTest
     {
         OpeningTemplateRepository templatesStorage;
+        Template template1;
+        Template template2;
+
         [TestInitialize]
         public void SetUp() {
             templatesStorage = new OpeningTemplateRepository();
+            template1 = new Template("Gate", 2, 0, 2, ComponentType.DOOR);
+            template2 = new Template("Hopper", 1, 1, 1, ComponentType.WINDOW);
         }
+        public void AddTemplates() {
+            
+            templatesStorage.Add(template1);
+            templatesStorage.Add(template2);
 
+        }
 
         [TestMethod]
         public void IsEmptyTest()
         {
             Assert.IsTrue(templatesStorage.IsEmpty());
         }
+
+        [TestMethod]
+        public void AddNotEmptyTest() {
+            AddTemplates();
+            Assert.IsFalse(templatesStorage.IsEmpty());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(TemplateAlreadyExistsException))]
+        public void AddRepeatedTest() {
+            AddTemplates();
+            templatesStorage.Add(template1);
+        }
+
     }
 }
