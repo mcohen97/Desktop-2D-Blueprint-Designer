8f266550c699fd56eb2a59fbfebc535846aa5386
Marcel Cohen <diegomarcel27@hotmail.com>
2018-06-13 15:22:57 -0300

    se crean pruebas para el repositorio de templates

--- Obligatorio1_Arancet_Cohen/UserRepositoryInterface/ITemplateRepository.cs
+++ Obligatorio1_Arancet_Cohen/UserRepositoryInterface/ITemplateRepository.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Logic.Domain;
 
-namespace UserRepositoryInterface
+namespace DomainRepositoryInterface
 {
-    interface ITemplateRepository
+    public interface ITemplateRepository
     {
-        
+        Template GetTemplateByName(string name); 
 
     }
 }
