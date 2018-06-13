8de19687a13f1cfbc3d5b615bbda21817145d32d
Marcel Cohen <diegomarcel27@hotmail.com>
2018-06-13 15:27:44 -0300

    se crea la clase de prueba

---
+++ Obligatorio1_Arancet_Cohen/DataAccessTest/OpeningTemplateRepositoryTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataAccess;
+
+namespace DataAccessTest
+{
+    [TestClass]
+    public class OpeningTemplateRepositoryTest
+    {
+        OpeningTemplateRepository templatesStorage;
+        [TestInitialize]
+        public void SetUp() {
+            templatesStorage = new OpeningTemplateRepository();
+        }
+
+
+        [TestMethod]
+        public void IsEmptyTest()
+        {
+            Assert.IsTrue(templatesStorage.IsEmpty());
+        }
+    }
+}
