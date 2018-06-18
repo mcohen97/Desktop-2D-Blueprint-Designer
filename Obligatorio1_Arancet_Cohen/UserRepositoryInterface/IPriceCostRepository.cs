252346a3d3f946bb07edf17f3cdb4400bcc33608
Marcel Cohen <diegomarcel27@hotmail.com>
2018-06-18 20:48:37 -0300

    se define interfaz para el repositorio de precios

---
+++ Obligatorio1_Arancet_Cohen/UserRepositoryInterface/IPriceCostRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRepositoryInterface
+{
+    interface IPriceCostRepository
+    {
+        float GetPrice(int componentType);
+
+        float GetCost(int componentType);
+
+        void SetPrice(int componentType, float price);
+
+        void SetCost(int componentType, float cost);
+
+        void AddCostPrice(int componentType, float cost,float price);
+    }
+}
