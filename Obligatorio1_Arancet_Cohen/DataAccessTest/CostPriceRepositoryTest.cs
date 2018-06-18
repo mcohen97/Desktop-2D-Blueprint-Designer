9408a0a192da55896623120dbc4023353c761734
Marcel Cohen <diegomarcel27@hotmail.com>
2018-06-18 20:59:24 -0300

    se agrega clase de prueba para repositorio de precios, con una prueba hecha

---
+++ Obligatorio1_Arancet_Cohen/DataAccessTest/CostPriceRepositoryTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DomainRepositoryInterface;
+using Logic.Domain;
+
+namespace DataAccessTest
+{
+    [TestClass]
+    public class CostPriceRepositoryTest
+    {
+        IPriceCostRepository prices = new PriceCostRepository();
+
+        [TestInitialize]
+        public void SetUp() {
+            prices.Clear();
+            prices.Add((int)ComponentType.WALL, 50, 100);
+
+        }
+
+        [TestInitialize]
+        public void GetPriceTest() {
+            float expectedResult = 100;
+            float actualResult=prices.GetPrice((int)ComponentType.WALL);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
