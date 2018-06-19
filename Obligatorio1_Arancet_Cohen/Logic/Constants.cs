01269b03479780458267de3742a32e042aa193f0
Marcel Cohen <diegomarcel27@hotmail.com>
2018-06-19 13:47:11 -0300

    se cambia nombre de la clase de constantes

---
+++ Obligatorio1_Arancet_Cohen/Logic/Constants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Domain
+{
+    public class Constants
+    {
+        public static readonly DateTime NEVER = DateTime.MinValue;
+
+        public static readonly float MAX_WALL_LENGTH = 5;
+
+        public static readonly float WALL_HEIGHT = 3;
+
+    }
+}
