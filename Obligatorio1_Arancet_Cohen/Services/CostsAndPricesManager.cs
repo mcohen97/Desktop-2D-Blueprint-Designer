227118126d76d6b69557ccb2cdef7a4892667210
Marcel Cohen <diegomarcel27@hotmail.com>
2018-06-18 22:02:43 -0300

    se crea la clase que se esta probando

---
+++ Obligatorio1_Arancet_Cohen/Services/CostsAndPricesManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logic.Domain;
+
+namespace Services
+{
+    public class CostsAndPricesManager
+    {
+        private Session currentSession;
+
+        public CostsAndPricesManager(Session aSession) {
+
+        }
+
+
+    }
+}
