44aab97529d074b2c7679a16403785002dbdece7
Marcel Cohen <diegomarcel27@hotmail.com>
2018-06-18 23:33:15 -0300

    se agregan dtos para reportes de precios y costos

---
+++ Obligatorio1_Arancet_Cohen/Logic/BlueprintCostReport.cs
@@ -0,0 +1,30 @@
+﻿using Logic.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    class BlueprintCostReport
+    {
+        private Dictionary<ComponentType, float> catalog;
+        public BlueprintCostReport()
+        {
+            catalog = new Dictionary<ComponentType, float>();
+        }
+
+        public void SetTotalCost(ComponentType material, float totalPrice)
+        {
+            catalog[material] = totalPrice;
+        }
+
+        public float GetTotalCost(ComponentType material)
+        {
+            return catalog[material];
+        }
+
+
+    }
+}
---
+++ Obligatorio1_Arancet_Cohen/Logic/BlueprintPriceReport.cs
@@ -0,0 +1,27 @@
+﻿using Logic.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Domain
+{
+    class BlueprintPriceReport
+    {
+        private Dictionary<ComponentType, float> catalog;
+        public BlueprintPriceReport() {
+            catalog = new Dictionary<ComponentType, float>();
+        }
+
+        public void SetTotalPrice(ComponentType material, float totalPrice) {
+            catalog[material] = totalPrice;
+        }
+
+        public float GetTotalPrice(ComponentType material) {
+            return catalog[material];
+        }
+
+
+    }
+}
