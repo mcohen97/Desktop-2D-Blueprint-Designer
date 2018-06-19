1a1cdcc6618d9677ef22077c951d9128dbbe2c7a
Marcel Cohen <diegomarcel27@hotmail.com>
2018-06-18 21:59:55 -0300

    se agrega testInitialice

--- Obligatorio1_Arancet_Cohen/ServicesTest/CostsAndPricesManagerTest.cs
+++ Obligatorio1_Arancet_Cohen/ServicesTest/CostsAndPricesManagerTest.cs
@@ -1,12 +1,40 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataAccess;
+using RepositoryInterface;
+using Logic.Domain;
+using Services;
+using DomainRepositoryInterface;
 
 namespace ServicesTest
 {
     [TestClass]
     public class CostsAndPricesManagerTest
     {
-       
+
+        IRepository<User> usersStorage;
+        UserAdministrator administrator;
+        SessionConnector connector;
+        Session currentSession;
+        IPriceCostRepository pricesNcosts;
+        CostsAndPricesManager manager;
+
+        [TestInitialize]
+        public void SetUp() {
+            usersStorage = new UserRepository();
+            usersStorage.Clear();
+            connector = new SessionConnector();
+            currentSession=connector.LogIn("admin", "admin");
+            administrator = new UserAdministrator(currentSession);
+            administrator.Add(new Designer("TestDesigner", "TestDesigner", "TestDesigner", "TestDesigner", DateTime.Now));
+            pricesNcosts = new PriceCostRepository();
+            pricesNcosts.Clear();
+            pricesNcosts.AddCostPrice((int)ComponentType.WALL, 50, 100);
+            manager = new CostsAndPricesManager(currentSession);
+        }
+
+        
+
         
     }
 }
