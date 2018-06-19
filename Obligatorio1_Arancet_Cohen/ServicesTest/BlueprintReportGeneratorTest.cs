5805119179b746c2a1c24e9967f12238e66eaf02
Marcel Cohen <diegomarcel27@hotmail.com>
2018-06-18 22:57:06 -0300

    se crea prueba para nuevo servicio que genera reportes sobre costos y precios de planos

---
+++ Obligatorio1_Arancet_Cohen/ServicesTest/BlueprintReportGeneratorTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Services;
+using Logic.Domain;
+
+namespace ServicesTest
+{
+    [TestClass]
+    public class BlueprintReportGeneratorTest
+    {
+        SessionConnector connector;
+        CostsAndPricesManager costsNPrices;
+        IBlueprint toReport;
+        BlueprintReportGenerator reporter;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            Session aSession = connector.LogIn("admin", "admin");
+            costsNPrices = new CostsAndPricesManager(aSession);
+            reporter = new BlueprintReportGenerator();
+        }
+    }
+}
