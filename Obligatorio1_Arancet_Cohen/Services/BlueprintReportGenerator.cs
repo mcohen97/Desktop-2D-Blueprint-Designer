010c49b9f3d847427aa174a3e17a7850cda319d5
Marcel Cohen <diegomarcel27@hotmail.com>
2018-06-18 23:42:34 -0300

    se implementa el metodo probado con la minima implementaci[on para que compile

--- Obligatorio1_Arancet_Cohen/Services/BlueprintReportGenerator.cs
+++ Obligatorio1_Arancet_Cohen/Services/BlueprintReportGenerator.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Logic.Domain;
 
 namespace Services
 {
     public class BlueprintReportGenerator
     {
+        public BlueprintPriceReport GeneratePriceReport()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
