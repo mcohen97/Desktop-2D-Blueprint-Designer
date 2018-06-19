beb98f203e4d1114d5e4c5f06ff1f9c9606f33ef
Marcel Cohen <diegomarcel27@hotmail.com>
2018-06-18 23:23:04 -0300

    se escriben metodos para inicializacion del contexto de pruebas y una prueba para el primer metodo a implementar

--- Obligatorio1_Arancet_Cohen/ServicesTest/BlueprintReportGeneratorTest.cs
+++ Obligatorio1_Arancet_Cohen/ServicesTest/BlueprintReportGeneratorTest.cs
@@ -2,6 +2,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Services;
 using Logic.Domain;
+using DomainRepositoryInterface;
+using DataAccess;
 
 namespace ServicesTest
 {
@@ -12,6 +14,7 @@ namespace ServicesTest
         CostsAndPricesManager costsNPrices;
         IBlueprint toReport;
         BlueprintReportGenerator reporter;
+        IPriceCostRepository storage;
 
         [TestInitialize]
         public void SetUp()
@@ -19,6 +22,39 @@ namespace ServicesTest
             Session aSession = connector.LogIn("admin", "admin");
             costsNPrices = new CostsAndPricesManager(aSession);
             reporter = new BlueprintReportGenerator();
+            toReport = new Blueprint(10,10,"TestBlueprint");
+            storage = new PriceCostRepository();
+            
         }
+
+        private void SetBlueprint() {
+            toReport.InsertWall(new Point(0, 0), new Point(5,0));
+            toReport.InsertWall(new Point(0, 0), new Point(0, 5));
+            toReport.InsertColumn(new Point(1, 1));
+            toReport.InsertColumn(new Point(1, 2));
+            Template temp = new Template("Slider", 2, 1, 2, ComponentType.WINDOW);
+            Opening window1 = new Window(new Point(0, 2), temp);
+            Opening window2 = new Window(new Point(2, 0), temp);
+            toReport.InsertOpening(window1);
+            toReport.InsertOpening(window2);
+        }
+
+        private void AddPrices() {
+            storage.Clear();
+            storage.AddCostPrice((int)ComponentType.WALL,50,100);
+            storage.AddCostPrice((int)ComponentType.BEAM,50,75);
+            storage.AddCostPrice((int)ComponentType.COLUMN,50,75);
+            storage.AddCostPrice((int)ComponentType.WINDOW,50,75);
+            storage.AddCostPrice((int)ComponentType.DOOR,50,100);
+        }
+
+        [TestMethod]
+        public void TestGetPricesWalls() {
+            BlueprintPriceReport report = costsNPrices.GeneratePriceReport();
+            int expectedResult = 1000;
+            int actualResult = report.WallsPrice;
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
     }
 }
